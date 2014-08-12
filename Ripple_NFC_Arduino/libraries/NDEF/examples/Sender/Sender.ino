2a966f77d5de95f9606825c3e4d96340507d5b78
Lucas <lucaswcron@gmail.com>
2014-08-12 12:05:21 -0400

    Added edited libraries

---
+++ Ripple_NFC_Arduino/libraries/NDEF/examples/Sender/Sender.ino
@@ -0,0 +1,47 @@
+// NFC Printer
+//
+// Use NFC peer to peer communication
+// The sketch depends on PN532 library(https://github.com/Seeed-Studio/PN532)
+// and NDEF library(https://github.com/don/NDEF)
+
+
+#include "SPI.h"
+#include "PN532_SPI.h"
+#include "snep.h"
+#include "NdefMessage.h"
+#include "SoftwareSerial.h"
+
+PN532_SPI pn532spi(SPI, 10);
+SNEP nfc(pn532spi);
+uint8_t ndefBuf[128];
+uint8_t recordBuf[128];
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("-------NFC Printer--------");
+}
+
+void loop()
+{
+
+    Serial.println("Get a message from Android");
+    int msgSize = nfc.read(ndefBuf, sizeof(ndefBuf));
+    if (msgSize > 0) {
+        NdefMessage msg  = NdefMessage(ndefBuf, msgSize);
+        msg.print();
+        Serial.println("\nSuccess");
+
+        NdefRecord record = msg.getRecord(0);
+        record.print();
+        int recordLength = record.getPayloadLength();
+        if (recordLength <= sizeof(recordBuf)) {
+            record.getPayload(recordBuf);
+                      
+            delay(2000);
+        }
+    } else {
+        Serial.println("failed");
+    }
+    delay(1000);
+}  
